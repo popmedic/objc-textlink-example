82f801eceb883245af05fb18e81b7e0fe1fbc92e
Kevin Scardina <kscardina@gmail.com>
2021-07-03 12:03:26 -0600

    updating the html for more fun.

--- TextLink/ViewController.m
+++ TextLink/ViewController.m
@@ -15,7 +15,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSString *text = @"this is my <a href=\"https://apple.com\">link</a>";
+    NSString *text = @"<p style=\"font-family:verdana;font-size:18px;\">this is my <a href=\"https://apple.com\">link</a><br/><br/>You can do <b>bold text</b>, you can do <i>italics</i>, and even <u>underline stuff</u>.<br/><br/>You can even do images!<br/><img src=\"https://avatars.githubusercontent.com/u/8209667?v=4\"><br/>All good text<ul  style=\"font-family:verdana;font-size:12px;\"><li>needs</li><li>some</li><li>lists</li></ul><br/><h1  style=\"font-family:verdana;\">Headers</h1><h2  style=\"font-family:verdana;\">And</h2><h3  style=\"font-family:verdana;\">smaller</h3><h4  style=\"font-family:verdana;\">headers</h4><br/><p style=\"font-family:verdana;font-size:14px;\">And the (scroll down)<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/></p><p style=\"font-family:verdana;font-size:22px;\">scroll bars come for free!</p>";
     NSData *data = [text dataUsingEncoding: NSUTF8StringEncoding];
     NSError *error;
     NSDictionary *options = @{
